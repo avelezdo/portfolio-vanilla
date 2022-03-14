4bc6314704a59a93ebe2b082005ec33dc491044c
avelezdo <albertovelezdev@gmail.com>
2022-03-14 21:00:53 +0100

    Se agrega proyecto 'tennis-championship'

--- js/onload.js
+++ js/onload.js
@@ -2,13 +2,24 @@ import { jobs } from '../public/data/jobs.json';
 import { renderJobs, attachShowEvent } from './render-work';
 import { renderSkills, attachSkillEvent, renderProjects, myProjects } from './render-projects';
 
+function sortById(a, b) {
+	if (a.id < b.id) {
+		return 1;
+	}
+	if (a.id > b.id) {
+		return -1;
+	}
+	return 0;
+}
+
 window.onload = async function () {
 	await renderSkills(0);
 	attachSkillEvent();
 	await renderJobs(jobs);
 	attachShowEvent();
+	const sortedProjects = myProjects.sort(sortById);
 	await renderProjects(
-		myProjects.filter((item, index) => index < 3),
+		sortedProjects.filter((item, index) => index < 3),
 		true
 	);
 	// attachHoverAnimationOnItems(navItems);
