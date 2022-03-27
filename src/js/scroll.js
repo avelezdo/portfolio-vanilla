c9307e379b846913041789313e544ae41a510085
avelezdo <albertovelezdev@gmail.com>
2022-03-27 15:59:07 +0200

    Limpieza

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -4,42 +4,40 @@ const aboutDestination = document.querySelector('.about'),
 	recommendationsDestination = document.querySelector('.recommendations'),
 	bandcampDestination = document.querySelector('.bandcamp'),
 	contactDestination = document.querySelector('.contact'),
-	navLink = document.querySelectorAll('.nav__link');
+	navLink = document.querySelectorAll('.nav__link')
 
 navLink.forEach((target) => {
 	target.addEventListener('click', () => {
-		console.log(target.dataset);
 		switch (target.dataset.name) {
 			case 'about':
-				scroll(aboutDestination);
-				break;
+				scroll(aboutDestination)
+				break
 			case 'work':
-				scroll(workDestination);
-				break;
+				scroll(workDestination)
+				break
 			case 'projects':
-				scroll(projectsDestination);
-				break;
+				scroll(projectsDestination)
+				break
 			case 'recommendations':
-				scroll(recommendationsDestination);
-				break;
+				scroll(recommendationsDestination)
+				break
 			case 'bandcamp':
-				scroll(bandcampDestination);
-				break;
+				scroll(bandcampDestination)
+				break
 			case 'contact':
-				scroll(contactDestination);
-				break;
+				scroll(contactDestination)
+				break
 			default:
-				break;
+				break
 		}
-	});
-});
+	})
+})
 
 function scroll(target) {
-	console.log(target);
 	target.scrollIntoView(
 		{
 			behavior: 'smooth',
 		},
 		5000
-	);
+	)
 }
