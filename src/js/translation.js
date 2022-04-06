449b5f7daba7d26f05e6e16c285cb3cd91fd7770
avelezdo <albertovelezdev@gmail.com>
2022-04-06 17:47:05 +0200

    Se agrega el enlace al CV en inglés cuando la web está en inglés

--- src/js/translation.js
+++ src/js/translation.js
@@ -19,11 +19,21 @@ translator.fetch(['en', 'es']).then(() => {
 	registerLanguageToggle()
 })
 
+function updateCVHref(language) {
+	let cvAnchor = document.querySelector('.CV-anchor')
+	let href = 'https://www.dropbox.com/s/cbrw880zet31l3d/Alberto%20V%C3%A9lez%20CV.pdf?raw=1'
+	if (language === 'en') {
+		href = 'https://www.dropbox.com/s/r3r70vt8ylpn3cg/English%20CV%20Alberto%20V%C3%A9lez.pdf?raw=1'
+	}
+	cvAnchor.href = href
+}
+
 function registerLanguageToggle() {
 	var select = document.querySelector('select')
 
 	select.addEventListener('change', (evt) => {
 		var language = evt.target.value
+		updateCVHref(language)
 		translator.translatePageTo(language)
 	})
 }
